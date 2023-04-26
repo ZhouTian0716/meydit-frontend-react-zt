4237455e18e02a4a29fe3d349056c1672e685a9b
ZhouTian0716 <egglord0716@gmail.com>
2023-04-27 01:20:35 +1000

    api config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,6 @@
 // import axios, { AxiosRequestConfig } from "axios";
-
 export const configOptions = {
-  apiAddress: import.meta.env.VITE_CONNECT_AWS_EC2
-    ? import.meta.env.VITE_AWS_EC2
-    : import.meta.env.VITE_LOCAL_DEV,
+  apiAddress: import.meta.env.VITE_LOCAL_DEV || import.meta.env.VITE_AWS_EC2,
 };
 
 // ZT-NOTE: alphaApi is an axios instance with configured headers
