b894cc84d62608ab0d0c1666c81463e88111446f
ZhouTian0716 <egglord0716@gmail.com>
2023-04-26 22:50:10 +1000

    fix front end env backend connect issue

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,10 +1,9 @@
 // import axios, { AxiosRequestConfig } from "axios";
+const backendEC2 = import.meta.env.VITE_AWS_EC2_BACKEND;
+const backendLocal = import.meta.env.VITE_LOCAL_BACKEND;
 
-// ZT-NOTE: import.meta.env.VITE_LOCAL_BACKEND is a variable that is set in the .env file
 export const configOptions = {
-  apiAddress: import.meta.env.VITE_CONNECT_AWS_EC2
-    ? import.meta.env.VITE_AWS_EC2_BACKEND
-    : import.meta.env.VITE_LOCAL_BACKEND,
+  apiAddress: backendLocal || backendEC2,
 };
 
 // ZT-NOTE: alphaApi is an axios instance with configured headers
