54cae75031376fa4555f6f18462e7315eced774d
ZhouTian0716 <egglord0716@gmail.com>
2023-05-01 00:40:15 +1000

    image upload to s3

--- src/api/aws.ts
+++ src/api/aws.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import { configOptions } from "../config/config";
 const { apiAddress } = configOptions;
 
-export const s3SecureUrl = async () => {
-  const res = await axios.get(`${apiAddress}/api/aws/s3`);
+export const s3SecureUrl = async (fileType:string) => {
+  const res = await axios.get(`${apiAddress}/api/aws/s3?fileType=${fileType}`);
   return res.data;
 };
 
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styles from "./Dashboard.module.scss";
 import axios from "axios";
-import { toast } from "react-toastify";
+
 // Redux
 import { getAccount, getToken } from "../../redux/reducers/authSlice";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
@@ -9,50 +9,50 @@ import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { s3SecureUrl } from "../../api/aws";
 import { validateFilesize } from "../../utils/helpers";
 
-// interface SelectedFile  {
-//   name?: string;
-//   size?: number;
-//   type?: string;
-//   preview?: string;
-// };
+const uploadToS3 = async (e: React.ChangeEvent<HTMLFormElement>) => {
+  const formData = new FormData(e.target);
+  // The input attribute name "projectImg" is used here
+  const file = formData.get("projectImg");
+  if (!file) {
+    return null;
+  }
+  // @ts-ignore
+  const fileType =encodeURIComponent(file.name).split('.')[1];
+  const url = await s3SecureUrl(fileType);
+  await axios.put(url, file);
+};
 
 const Dashboard = () => {
   const [selectedFile, setSelectedFile] = useState<any>();
   // Redux
   const loginUser = useAppSelector(getAccount);
 
-  const createProject = async (e: React.SyntheticEvent<HTMLFormElement>) => {
+  const createProject = async (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(selectedFile);
-    // get secure url from backend
-    // const url = await s3SecureUrl();
-    // console.log("Uploading project", url);
-    // upload image to s3 with secure url
-    // await axios.put(url, {
-    //   headers: {
-    //     "Content-Type": "multipart/form-data",
-    //   },
-
-    // });
+    await uploadToS3(e);
     // post request to server to store project
   };
+
   const onAddImage = (e: React.ChangeEvent<HTMLInputElement>) => {
     // We prevent massive images from being uploaded
     if (!e.target.files) return;
     const file = e.target.files[0];
-    const { size } = file;
-    // console.log(file.size)
     const maxMB = 15;
-    const sizeOK = validateFilesize(size, maxMB);
+    const sizeOK = validateFilesize(file.size, maxMB);
     if (!sizeOK) return;
     setSelectedFile(file);
   };
+
   return (
     <div className={styles.dashboard}>
       <h1>Hello {loginUser.first_name || loginUser.email?.split("@")[0]}</h1>
       <p>Wanna post a new project today?</p>
       <form className={styles.authForm} onSubmit={createProject}>
-        <input type="file" onChange={onAddImage} />
+        <input
+          type="file"
+          onChange={onAddImage}
+          name="projectImg"
+        />
         {selectedFile && (
           <div className={styles.imgContainer}>
             <img
